7051e8a161e434a1d58100837ded3e6e74017d19
marrasluca <lmarras10@gmail.com>
2025-01-29 15:55:26 +0100

    add: for

---
+++ main.js
@@ -0,0 +1,10 @@
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+//  generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
+const htmlList = document.getElementById('email')
+
+// ciclo for che verrà eseguito 10 volte
+for( i=0; i < 10; i++){
+
+}

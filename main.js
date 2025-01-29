6c7768a308cb787b81d74670a9cc0a16ba5d0911
marrasluca <lmarras10@gmail.com>
2025-01-29 16:06:20 +0100

    end

--- main.js
+++ main.js
@@ -7,4 +7,16 @@ const htmlList = document.getElementById('email')
 // ciclo for che verrà eseguito 10 volte
 for( i=0; i < 10; i++){
 
+    // richiesta GET all'API per ottenere un indirizzo email casuale
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail", {method: "GET"})
+    .then(response => response.json())
+    .then( data => {
+
+        console.log(data.response);
+
+        //aggiunge l'email ricevuta alla lista HTML
+        htmlList.innerHTML += `<li>${data.response}</li>`
+
+    })
+
 }
